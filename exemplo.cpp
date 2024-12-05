f5dc1b4204f54d950c4fd339ff4212fa0b1c3dd5
Renanservo <renanelayana@gmail.com>
2024-12-04 22:14:41 -0300

    feat:primeira versão

---
+++ exemplo.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int a;
+    int b;
+
+    cout << "EXEMPLO INICIAL DE APLICATIVO" << endl;
+    cout << "DIGITE UM NUMERO: ";
+    cin >> a;
+    cout << "DIGETE OUTRO NUMERO: ";
+    cin >> b;
+    cout <<"A SOMA DOS NUMEROS e: " << a + b << endl;
+
+    system("pause");
+    return 0;
+}
